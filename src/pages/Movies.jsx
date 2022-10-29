c122628512be7641019400cce95ec8d7c93a53c1
gflo1997 <gonzalo.fj@hotmail.com>
2022-10-28 22:59:01 -0400

    Commit

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -5,42 +5,35 @@ import axios from "axios";
 const Movies = () => {
   const { movie } = useParams();
   const [movies, setMovies] = useState([]);
-  const [searchMovie, setSearchMovie] = useState(movie);
 
-  function onSearch() {
-    fetchMovies(searchMovie);
-  }
-
-  async function fetchMovies(movie) {
-    const { data } = await axios.get(
-      `https://www.omdbapi.com/?s=${movie}&apikey=ddf07056`
-    );
-    setMovies(data);
+  
   }
  
   useEffect(() => {
+    async function fetchMovies() {
+    const { data } = await axios.get(
+      `https://www.omdbapi.com/?s=${movie}&apikey=ddf07056`
+    );
+
+    setMovies(data);
+    }
     fetchMovies();
+
   }, []);
 
 
 
   return (
-    <div>
+    <>
       <div className="SearchBar">
         <input
           className="input"
           type="text"
           value={searchMovie}
           placeholder="Titles, people, genres"
-          onChange={(event) => setSearchMovie(event.target.value)}
-          onKeyPress={(event) => {
-            if (event.key === "Enter") {
-              onSearch();
-            }
-          }}
 
         />
-        <button onClick={() => onSearch()} className="searchButton">
+        <button className="searchButton">
           <img
             className="searchIcon"
             src="https://static.vecteezy.com/system/resources/previews/000/442/657/non_2x/vector-search-icon.jpg"
@@ -48,17 +41,17 @@ const Movies = () => {
           />
         </button>
       </div>
-
-
       <div className="card-container">
+
+      {movies.map((card) => (
         <div className="card">
           <img
             className="movie-img"
-            src="https://cdn.pocket-lint.com/r/s/1200x630/assets/images/148310-tv-feature-what-order-should-you-watch-the-fast-and-furious-films-in-image1-rzgajwfo2x.jpg"
+            src={card.Poster}
             alt=""
           />
           <div className="movie-container">
-            <h1>Title</h1>
+            <h1>{card.Title}</h1>
             <p>
               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius
               optio quos earum! Iure, quos corporis! Pariatur esse quas nemo est
@@ -67,94 +60,10 @@ const Movies = () => {
             </p>
             <h2>3.5</h2>
           </div>
-        </div>
-        <div className="card">
-          <img
-            className="movie-img"
-            src="https://cdn.pocket-lint.com/r/s/1200x630/assets/images/148310-tv-feature-what-order-should-you-watch-the-fast-and-furious-films-in-image1-rzgajwfo2x.jpg"
-            alt=""
-          />
-          <div className="movie-container">
-            <h1>Title</h1>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius
-              optio quos earum! Iure, quos corporis! Pariatur esse quas nemo est
-              ad dolor facere, quod molestias, quia voluptatum officiis?
-              Consequatur, asperiores!
-            </p>
-            <h2>3.5</h2>
-          </div>
-        </div>
-        <div className="card">
-          <img
-            className="movie-img"
-            src="https://cdn.pocket-lint.com/r/s/1200x630/assets/images/148310-tv-feature-what-order-should-you-watch-the-fast-and-furious-films-in-image1-rzgajwfo2x.jpg"
-            alt=""
-          />
-          <div className="movie-container">
-            <h1>Title</h1>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius
-              optio quos earum! Iure, quos corporis! Pariatur esse quas nemo est
-              ad dolor facere, quod molestias, quia voluptatum officiis?
-              Consequatur, asperiores!
-            </p>
-            <h2>3.5</h2>
-          </div>
-        </div>
-        <div className="card">
-          <img
-            className="movie-img"
-            src="https://cdn.pocket-lint.com/r/s/1200x630/assets/images/148310-tv-feature-what-order-should-you-watch-the-fast-and-furious-films-in-image1-rzgajwfo2x.jpg"
-            alt=""
-          />
-          <div className="movie-container">
-            <h1>Title</h1>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius
-              optio quos earum! Iure, quos corporis! Pariatur esse quas nemo est
-              ad dolor facere, quod molestias, quia voluptatum officiis?
-              Consequatur, asperiores!
-            </p>
-            <h2>3.5</h2>
-          </div>
-        </div>
-        <div className="card">
-          <img
-            className="movie-img"
-            src="https://cdn.pocket-lint.com/r/s/1200x630/assets/images/148310-tv-feature-what-order-should-you-watch-the-fast-and-furious-films-in-image1-rzgajwfo2x.jpg"
-            alt=""
-          />
-          <div className="movie-container">
-            <h1>Title</h1>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius
-              optio quos earum! Iure, quos corporis! Pariatur esse quas nemo est
-              ad dolor facere, quod molestias, quia voluptatum officiis?
-              Consequatur, asperiores!
-            </p>
-            <h2>3.5</h2>
-          </div>
-        </div>
-        <div className="card">
-          <img
-            className="movie-img"
-            src="https://cdn.pocket-lint.com/r/s/1200x630/assets/images/148310-tv-feature-what-order-should-you-watch-the-fast-and-furious-films-in-image1-rzgajwfo2x.jpg"
-            alt=""
-          />
-          <div className="movie-container">
-            <h1>Title</h1>
-            <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius
-              optio quos earum! Iure, quos corporis! Pariatur esse quas nemo est
-              ad dolor facere, quod molestias, quia voluptatum officiis?
-              Consequatur, asperiores!
-            </p>
-            <h2>3.5</h2>
-          </div>
-        </div>
       </div>
-    </div>
+      ))}
+      </div>
+    </>
   );
 };
 
